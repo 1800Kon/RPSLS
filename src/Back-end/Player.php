97c11370d689b0b4154497fa48f08b365ce3a778
Joel Kleibs <joel.kleibs@student.nhlstenden.com>
2021-03-24 16:42:25 +0100

    Update directories and added Player.php

---
+++ src/Back-end/Player.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class Player
+{
+    //properties
+    public $name;
+    public $colour;
+    public $points;
+
+    //methods
+    function bet($input)
+    {
+
+    }
+
+    function add_points($input)
+    {
+        
+    }
+
+    //getters and setters
+    function set_name($name)
+    {
+        $this->name = $name;
+    }
+
+    function get_name()
+    {
+        return $this->name;
+    }
+}
+
+?>
