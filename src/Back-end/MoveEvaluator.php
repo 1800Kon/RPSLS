ce20ca77fa974824e14ba566eae09b3b65186e35
costa <costandinoh@gmail.com>
2021-03-24 18:15:25 +0100

    Created the check for determining the winner

--- src/Back-end/MoveEvaluator.php
+++ src/Back-end/MoveEvaluator.php
@@ -19,7 +19,17 @@ class MoveEvaluator
             {
                 if($player != $comparedPlayer)
                 {
-
+                    switch ($this->matrix($player->getMove(), $comparedPlayer->getMove()))
+                    {
+                        case true:
+                            //TODO: player wins and comparedPlayer loses
+                            break;
+                        case false:
+                            //TODO: player loses and comparedPlayer wins
+                            break;
+                        default:
+                            //TODO: player and comparedPlayer draw
+                    }
                 }
             }
         }
@@ -28,9 +38,91 @@ class MoveEvaluator
     //1 = rock, 2 = paper, 3 = scissors, 4 = lizard, 5 = spock
     public function matrix($move1, $move2)
     {
-        if($move1 == 1)
+        //Handling the moves if move1 beats move2
+        if ($move1 == 1 && $move2 == 3)
         {
-        
+            return true;
+        }
+        elseif($move1 == 1 && $move2 == 4)
+        {
+             return true;
+        }
+        elseif ($move1 == 2 && $move2 == 1)
+        {
+            return true;
+        }
+        elseif($move1 == 2 && $move2 == 5)
+        {
+            return true;
+        }
+        elseif($move1 == 3 && $move2 == 2)
+        {
+            return true;
+        }
+        elseif($move1 == 3 && $move2 == 4)
+        {
+            return true;
+        }
+        elseif($move1 == 4 && $move2 == 2)
+        {
+            return true;
+        }
+        elseif($move1 == 4 && $move2 == 5)
+        {
+            return true;
+        }
+        elseif($move1 == 5 && $move2 == 1)
+        {
+            return true;
+        }
+        elseif($move1 == 5 && $move2 == 3)
+        {
+            return true;
+        }
+        //Handling the moves if move1 loses against move2
+        elseif ($move1 == 1 && $move2 == 2)
+        {
+            return false;
+        }
+        elseif($move1 == 1 && $move2 == 5)
+        {
+            return false;
+        }
+        elseif ($move1 == 2 && $move2 == 3)
+        {
+            return false;
+        }
+        elseif($move1 == 2 && $move2 == 4)
+        {
+            return false;
+        }
+        elseif($move1 == 3 && $move2 == 1)
+        {
+            return false;
+        }
+        elseif($move1 == 3 && $move2 == 5)
+        {
+            return false;
+        }
+        elseif($move1 == 4 && $move2 == 1)
+        {
+            return false;
+        }
+        elseif($move1 == 4 && $move2 == 3)
+        {
+            return false;
+        }
+        elseif($move1 == 5 && $move2 == 2)
+        {
+            return false;
+        }
+        elseif($move1 == 5 && $move2 == 4)
+        {
+            return false;
+        }
+        else
+        {
+            return null;
         }
     }
 
